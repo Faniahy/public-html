4edba47d6b54855ee92d4d313bb27b0b3255e360
faniahy <ralisonfaniahy@gmail.com>
2019-07-19 10:12:53 +0300

    Modif avec des astuces d'animations

--- Mon Site/js/main.js
+++ Mon Site/js/main.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
 	$('.afficher').click(function(){
 
 		$('h1').css('color','red');
+		$('.texte').html("<div>Je suis un dinosaure</div>");
+		$(this).hide();
 
 		/*console.log("La couleur vient de changer en rouge!");
 		alert("La couleur vient de changer en rouge!");*/
 
 		/*$('h1').hide();*/
+
+		/*Pour les animation, on doit faire $().animate();*/
 
 	});
 
