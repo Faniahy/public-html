b492e7c533fb4955e0a2aa34ecaa39d8c8389e81
faniahy <ralisonfaniahy@gmail.com>
2019-07-24 14:59:54 +0300

    grafikart PHP

---
+++ Trav-php/demo.php
@@ -0,0 +1,10 @@
+<?php
+	$prenom = 'Marc';
+	$nom = 'Doe';
+	$note1 = 10;
+	$note2 = 20;
+
+	$moy = ($note1 + $note2) / 2;
+
+	echo 'Bonjour '.$prenom.' '.$nom.' vous avez eu '.$moy.' de moyenne';
+?>
