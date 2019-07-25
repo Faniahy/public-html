77c06f2bbf825d913edf1e7832ecc6f051bcb307
faniahy <ralisonfaniahy@gmail.com>
2019-07-25 17:05:28 +0300

    Modification avec comparaison 3 nombre

--- Trav-php/demo.php
+++ Trav-php/demo.php
@@ -4,7 +4,7 @@
 	echo $notes[3]; // 11*/
 
 	/*$eleve = ['Marc', 'Doe', [10,20,40]];*/
-	$eleve = [
+	/*$eleve = [
 		[
 			'nom' => 'Doe', 
 			'prenom' => 'Marc',
@@ -16,10 +16,28 @@
 			'notes' => [15, 14, 13]
 		]
 		
-	];
+	];*/
 
-	/* echo $eleve[2][1]; // 20 */
+	/* echo $eleve[2][1]; // 20 
 	/* print_r($eleve['note']);  // 20 */
-	echo $eleve[1]['notes'][1]; // 14
+	/*echo $eleve[1]['notes'][1]; // 14*/
+
+	//$notes = readline("Entrez votre note: "); // Afaka mampiditra am clavier
+	/*if($notes >= 10){
+		echo "Vous avez la moyenne";
+	} else {
+		echo 'Désoler vous n\'avez pas la moyenne';
+	}*/
+	 function plusPetit($n,$a,$b){
+		if($n<$a){
+	        if($a<$b) echo $n;
+	    }else{
+	        if($a>$b) echo $b;
+	        else echo $a;
+	    }
+    }
+
+    plusPetit(100,1000,10000);
+
 
 ?>
